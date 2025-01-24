70ae645b831d7df01865ffc06dbf5b436b1d7dff
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-24 18:33:11 -0300

    Create products route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import { productsRouter } from "@modules/products/routes/products.routes";
 import { Router, Request, Response } from "express";
 
 const routes = Router();
@@ -5,5 +6,7 @@ const routes = Router();
 routes.get("/", (request: Request, response: Response) => {
   return response.json({ message: "Olá, dev!" });
 });
+
+routes.use("/products", productsRouter);
 
 export default routes;
