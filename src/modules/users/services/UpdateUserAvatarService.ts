5a7514c5277fb116f964ecc5e2fa2e52fc78b99e
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-27 18:36:20 -0300

    Create UpdateUserAvatarService

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,45 @@
+import { inject, injectable } from "tsyringe";
+import { AppError } from "@shared/errors/AppError";
+import { IUsersRepository } from "../repositories/IUsersRepository";
+import User from "../entities/User";
+import path from "node:path";
+import uploadConfig from "@config/upload";
+import fs from "node:fs";
+
+type UpdateUserAvatarServiceDTO = {
+  user_id: string;
+  avatarFilename: string;
+};
+
+@injectable()
+export class UpdateUserAvatarService {
+  constructor(
+    @inject("UsersRepository")
+    private userRepository: IUsersRepository,
+  ) {}
+
+  public async execute({
+    user_id,
+    avatarFilename,
+  }: UpdateUserAvatarServiceDTO): Promise<User> {
+    const user = await this.userRepository.getById(user_id);
+
+    if (!user) {
+      throw new AppError("User not found.");
+    }
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await this.userRepository.update(user);
+
+    return user;
+  }
+}
