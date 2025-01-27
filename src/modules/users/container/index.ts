67ae8c6eec8fc73d96e5c7fc383e54ef8bddb9ef
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-27 18:09:43 -0300

    Add SessionsController in container

--- src/modules/users/container/index.ts
+++ src/modules/users/container/index.ts
@@ -1,7 +1,7 @@
 import { container } from "tsyringe";
 import { UsersRepository } from "../repositories/UsersRepository";
 import { UsersController } from "../controllers/UsersController";
-import { SessionsController } from "../controllers/SessionController";
+import { SessionsController } from "../controllers/SessionsController";
 
 container.registerSingleton("UsersRepository", UsersRepository);
 container.registerSingleton("UsersController", UsersController);
