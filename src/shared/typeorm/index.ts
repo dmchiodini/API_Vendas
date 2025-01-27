e8814742add62e1d27195737c4515b17a1d1cc65
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-27 11:32:48 -0300

    Create user entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,6 +1,8 @@
 import { DataSource } from "typeorm";
 import { CreateProductsTable1737726624976 } from "./migrations/1737726624976-CreateProductsTable";
 import Product from "@modules/products/entities/Product";
+import { CreateUsers1737986825854 } from "./migrations/1737986825854-CreateUsers";
+import User from "@modules/users/entities/User";
 
 export const dataSource = new DataSource({
   type: "postgres",
@@ -9,6 +11,6 @@ export const dataSource = new DataSource({
   username: "postgres",
   password: "docker",
   database: "vendasapi",
-  entities: [Product],
-  migrations: [CreateProductsTable1737726624976],
+  entities: [Product, User],
+  migrations: [CreateProductsTable1737726624976, CreateUsers1737986825854],
 });
